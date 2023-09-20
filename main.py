e99f0375504e30a36476835aaeda9208a5665a2a
tbumgard <tbumgard@gmail.com>
2023-09-20 13:32:03 -0400

    Final draft of BookBot to submit to boot.dev

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-with open("books/frankenstein.txt") as f:
-    file_contents = f.read()
+__BOOK = "books/frankenstein.txt"  ##the path and file that we want to open
+
+with open(__BOOK) as f: ##open the file
+    file_contents = f.read()    
 
 def Count_Words(book): ##returns the number of words in a book
     return len(book.split())
@@ -16,14 +18,21 @@ def List_of_Letters_Counts(book):
     return letters_counts
 
 def main():
-    print(file_contents)
-    
     word_count = Count_Words(file_contents) 
     list_of_letter_counts = List_of_Letters_Counts(file_contents)
+    sorted_list_of_letters = []
+    
+    print(f"--- Begin report of {__BOOK} ---")
+    print(f"{word_count} words found in the document")
+    
+    for letter in list_of_letter_counts: ##grab all the characters(letters) and put them into a list to sort
+        sorted_list_of_letters.append(letter)
 
-    print(f"There are {word_count} words in this book.")
+    sorted_list_of_letters.sort()  ##sort the characters list
 
-    for letter in list_of_letter_counts:
-        print(f"{letter}: {list_of_letter_counts[letter]}")
+    for letter in sorted_list_of_letters: ##iterate through the sorted list
+        if letter.isalpha(): ##remove non alphabet characters from the printout
+            print(f"The '{letter}' character was found {list_of_letter_counts[letter]} times") ##print the results
 
+    print("--- End report ---")
 main()
