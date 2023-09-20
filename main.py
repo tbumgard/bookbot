1dec65292aa63c24f348257e878d95f408e85cb8
tbumgard <tbumgard@gmail.com>
2023-09-20 13:37:25 -0400

    Final draft of BookBot to submit to boot.dev

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-__BOOK = "books/frankenstein.txt"  ##the path and file that we want to open
+##the path and file that we want to open
+__BOOK = "books/frankenstein.txt"  
 
-with open(__BOOK) as f: ##open the file
+##open the file
+with open(__BOOK) as f: 
     file_contents = f.read()    
 
-def Count_Words(book): ##returns the number of words in a book
+##returns the number of words in a book
+def Count_Words(book): 
     return len(book.split())
 
+##returns a dictionary of letters and the number of times they show up in book. tuple of ['string'] : count(int)
 def List_of_Letters_Counts(book):
     letters_counts = {}
     lower_case_book = book.lower()
@@ -17,6 +21,7 @@ def List_of_Letters_Counts(book):
 
     return letters_counts
 
+##main program
 def main():
     word_count = Count_Words(file_contents) 
     list_of_letter_counts = List_of_Letters_Counts(file_contents)
@@ -25,14 +30,20 @@ def main():
     print(f"--- Begin report of {__BOOK} ---")
     print(f"{word_count} words found in the document")
     
-    for letter in list_of_letter_counts: ##grab all the characters(letters) and put them into a list to sort
+    ##grab all the characters(letters) and put them into a list to sort
+    for letter in list_of_letter_counts: 
         sorted_list_of_letters.append(letter)
 
-    sorted_list_of_letters.sort()  ##sort the characters list
+    ##sort the characters list  
+    sorted_list_of_letters.sort()  
 
-    for letter in sorted_list_of_letters: ##iterate through the sorted list
-        if letter.isalpha(): ##remove non alphabet characters from the printout
-            print(f"The '{letter}' character was found {list_of_letter_counts[letter]} times") ##print the results
+    ##iterate through the sorted list
+    for letter in sorted_list_of_letters: 
+        ##remove non alphabet characters from the printout
+        if letter.isalpha(): 
+            ##print the results
+            print(f"The '{letter}' character was found {list_of_letter_counts[letter]} times") 
 
     print("--- End report ---")
+
 main()
